4a1f648ce4e074dabfc9fec637109d9b62311ae2
Albina Ishmukhametova <albina.faritovna.i@gmail.com>
2017-02-09 12:35:39 +0300

    test2.php was updated

--- test2.php
+++ test2.php
@@ -2,8 +2,5 @@
 $id = '6'; // id элемента списка, взят просто для примера
 $task = $_POST['tasktext'];
 
-$data['id'] = $id;
-$data['task'] = $task;
-
 echo json_encode(array('id'=>$id,'task'=>$task));
 ?>
